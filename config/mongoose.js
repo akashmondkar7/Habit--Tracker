9c65b155333fad13d88208ae417b897a809dbe64
Your Name <akashmondkar03@gmail.com>
2026-05-31 23:39:21 +0530

    save

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,11 +2,12 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
+    await mongoose.connect(process.env.MONGO_URI);
     if (!process.env.MONGO_URI) {
       throw new Error("MONGO_URI is missing");
     }
 
-    await mongoose.connect(process.env.MONGO_URI);
+   
 
     console.log("MongoDB Connected Successfully");
   } catch (error) {
